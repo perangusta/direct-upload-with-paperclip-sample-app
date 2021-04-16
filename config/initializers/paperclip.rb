993d308db2940dfab0f8885ba80defd8f758b293
Jean-Michel Gigault <jgigault@student.42.fr>
2021-04-16 13:51:13 +0200

    configure AWS S3 file storage

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,6 +2,19 @@ Paperclip::Attachment.default_options.update(
   { hash_secret: 'my paperclip hash secret' }
 )
 
+Paperclip::Attachment.default_options.update(
+  {
+    storage: :s3,
+    s3_permissions: 'private',
+    s3_credentials: {
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+    },
+    s3_region: ENV.fetch('AWS_REGION'),
+    bucket: ENV.fetch('S3_BUCKET_NAME')
+  }
+)
+
 Paperclip.interpolates :tenant_directory  do |attachment, _style|
   attachment.instance.tenant_directory
 end
